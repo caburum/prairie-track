36e7f8dfeb3a2802978cf7c0fc39446c14e7bdeb
Calum Dingwall <caburum@users.noreply.github.com>
2025-09-09 19:05:59 -0500

    fix for prairielearn ui tweaks & fix reload button

--- content.js
+++ content.js
@@ -6,15 +6,30 @@
     return;
   }
   if (window.location.search.includes("reload")) {
-    window.close();
+    const div = document.createElement("div");
+    div.style = `position: fixed; inset: 0; z-index: 9999; background-color:#fff;`;
+    div.innerHTML = /* html */ `
+      <div style="display: flex; flex-direction: column; align-items: center; justify-content:center;height:100vh;">
+        <h3>Reloading PrairieTrack assessments...</h3>
+        <p>This window will close automatically. If it doesn't, please close it manually.</p>
+      </div>
+    `;
+    document.body.appendChild(div);
   }
   const reloadRows = /* js */ `
-    window.localStorage.clear();
-    document.querySelectorAll(&quot;a[href^='/pl/course_instance/']:not([href$='/'])&quot;).forEach((a) => {
-      window.open(a.href + '/assessments?reload');
+    Object.keys(window.localStorage).forEach(key =>
+      key.startsWith('prairieTrack-') ? window.localStorage.removeItem(key) : null
+    );
+    let courses = document.querySelectorAll(\`a[href^='/pl/course_instance/']:not([href$='/'])\`),
+      i = courses.length;
+    courses.forEach((a) => {
+      // todo: use fetch instead of window.open
+      if (!window.open(a.href + '/assessments?reload', '', 'popup,width=100,height=100'))
+        alert('Please allow popups for this website to enable the reload feature.');
     });
     window.addEventListener('storage', function () {
-      window.location.reload();
+      console.log('storage event detected');
+      if (--i === 0) window.location.reload();
     });
   `;
   try {
@@ -24,14 +39,20 @@
     let courseHref = "";
     if (isAssessmentsPage) {
       courseCode = document
-        .querySelector("#main-nav > li.nav-item.mr-4 > span")
-        .innerHTML.split(",")[0];
+        .querySelector("#main-nav > li:first-child")
+        .innerText.trim().split(",")[0];
       courseHref = window.location.href.split("?")[0];
       rows = Array.from(
-        document.querySelectorAll("#content > div > table > tbody > tr:has(td)")
+        document.querySelectorAll("#content table > tbody > tr:has(td)")
       );
       rows = rows.map((row) => row.cloneNode(true));
-      rows = rows.filter((row) => row.children[2].innerHTML.includes("until"));
+      rows = rows.filter((row) => {
+        let grade = parseFloat(row.children[3].innerText);
+        return (
+          row.children[2].innerHTML.includes("until") &&
+          (isNaN(grade) || grade < 100)
+        );
+      });
     } else if (isPrairieLearnPage) {
       const keys = Object.keys(window.localStorage).filter((key) =>
         key.startsWith("prairieTrack-")
@@ -53,7 +74,7 @@
     parent.insertBefore(div, parent.firstChild);
     div.innerHTML = /* html */ `
       <div class="card-header bg-primary text-white d-flex align-items-center">PrairieTrack<sup>beta</sup><small style="opacity: 50%">&nbsp;by Anthony Du</small>
-        <button type="button" class="btn btn-light btn-sm ml-auto ${
+        <button type="button" class="btn btn-light btn-sm ms-auto ${
           isPrairieLearnPage ? "" : "d-none"
         }" onclick="${reloadRows}">
           <svg class="svg-inline--fa" fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 487.23 487.23"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M55.323,203.641c15.664,0,29.813-9.405,35.872-23.854c25.017-59.604,83.842-101.61,152.42-101.61 c37.797,0,72.449,12.955,100.23,34.442l-21.775,3.371c-7.438,1.153-13.224,7.054-14.232,14.512 c-1.01,7.454,3.008,14.686,9.867,17.768l119.746,53.872c5.249,2.357,11.33,1.904,16.168-1.205 c4.83-3.114,7.764-8.458,7.796-14.208l0.621-131.943c0.042-7.506-4.851-14.144-12.024-16.332 c-7.185-2.188-14.947,0.589-19.104,6.837l-16.505,24.805C370.398,26.778,310.1,0,243.615,0C142.806,0,56.133,61.562,19.167,149.06 c-5.134,12.128-3.84,26.015,3.429,36.987C29.865,197.023,42.152,203.641,55.323,203.641z"></path> <path d="M464.635,301.184c-7.27-10.977-19.558-17.594-32.728-17.594c-15.664,0-29.813,9.405-35.872,23.854 c-25.018,59.604-83.843,101.61-152.42,101.61c-37.798,0-72.45-12.955-100.232-34.442l21.776-3.369 c7.437-1.153,13.223-7.055,14.233-14.514c1.009-7.453-3.008-14.686-9.867-17.768L49.779,285.089 c-5.25-2.356-11.33-1.905-16.169,1.205c-4.829,3.114-7.764,8.458-7.795,14.207l-0.622,131.943 c-0.042,7.506,4.85,14.144,12.024,16.332c7.185,2.188,14.948-0.59,19.104-6.839l16.505-24.805 c44.004,43.32,104.303,70.098,170.788,70.098c100.811,0,187.481-61.561,224.446-149.059 C473.197,326.043,471.903,312.157,464.635,301.184z"></path></g></g></g></svg>
@@ -65,7 +86,7 @@
           <tr>
             <th colspan="${
               isAssessmentsPage ? 2 : 3
-            }" data-testid="assessment-group-heading">
+            }">
               All Upcoming Assessments
             </th>
             <th class="text-center">Due Time</th>
@@ -165,7 +186,7 @@
     parent.insertBefore(div, parent.firstChild);
     div.innerHTML = /* html */ `
       <div class="card-header bg-primary text-white d-flex align-items-center">PrairieTrack<sup>beta</sup><small style="opacity: 50%">&nbsp;by Anthony Du</small>
-        <button type="button" class="btn btn-light btn-sm ml-auto ${
+        <button type="button" class="btn btn-light btn-sm ms-auto ${
           isPrairieLearnPage ? "" : "d-none"
         }" onclick="${reloadRows}">
           <svg class="svg-inline--fa" fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 487.23 487.23"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M55.323,203.641c15.664,0,29.813-9.405,35.872-23.854c25.017-59.604,83.842-101.61,152.42-101.61 c37.797,0,72.449,12.955,100.23,34.442l-21.775,3.371c-7.438,1.153-13.224,7.054-14.232,14.512 c-1.01,7.454,3.008,14.686,9.867,17.768l119.746,53.872c5.249,2.357,11.33,1.904,16.168-1.205 c4.83-3.114,7.764-8.458,7.796-14.208l0.621-131.943c0.042-7.506-4.851-14.144-12.024-16.332 c-7.185-2.188-14.947,0.589-19.104,6.837l-16.505,24.805C370.398,26.778,310.1,0,243.615,0C142.806,0,56.133,61.562,19.167,149.06 c-5.134,12.128-3.84,26.015,3.429,36.987C29.865,197.023,42.152,203.641,55.323,203.641z"></path> <path d="M464.635,301.184c-7.27-10.977-19.558-17.594-32.728-17.594c-15.664,0-29.813,9.405-35.872,23.854 c-25.018,59.604-83.843,101.61-152.42,101.61c-37.798,0-72.45-12.955-100.232-34.442l21.776-3.369 c7.437-1.153,13.223-7.055,14.233-14.514c1.009-7.453-3.008-14.686-9.867-17.768L49.779,285.089 c-5.25-2.356-11.33-1.905-16.169,1.205c-4.829,3.114-7.764,8.458-7.795,14.207l-0.622,131.943 c-0.042,7.506,4.85,14.144,12.024,16.332c7.185,2.188,14.948-0.59,19.104-6.839l16.505-24.805 c44.004,43.32,104.303,70.098,170.788,70.098c100.811,0,187.481-61.561,224.446-149.059 C473.197,326.043,471.903,312.157,464.635,301.184z"></path></g></g></g></svg>
@@ -181,5 +202,8 @@
         </tbody>
       </table>
     `;
+  }
+  if (window.location.search.includes("reload")) {
+    window.close();
   }
 })();
